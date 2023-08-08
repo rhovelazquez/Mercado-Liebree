0087a2f621f5fe0018c5c532eeba32eb1116877b
rhovelazquez <rocyvelazquez2021@gmail.com>
2023-08-08 09:30:36 -0300

    mercado liebre completo quiero creer

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require ('express');
+const app= express();
+const path = require('path');
+
+const PORT =3030;
+app.use(express.static(path.resolve(__dirname,'public')));
+
+app.listen(PORT,()=> console.log(`Server Up: ${PORT}`));
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./views/home.html'));
+});
+
+app.get('/register.html',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./views/register.html'));
+});
+
+app.get('/login.html',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./views/login.html'));
+});
+app.post('/',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./views/home.html'));
+});
